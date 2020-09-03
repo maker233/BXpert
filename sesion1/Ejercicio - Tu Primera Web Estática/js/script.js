8bc2dd9452ee6d97a5d3c08ff77b263ae107fa03
Javier Dev <javiermaker@gmail.com>
2020-09-03 07:55:27 +0200

    first commit

---
+++ sesion1/Ejercicio - Tu Primera Web Estática/js/script.js
@@ -0,0 +1,9 @@
+
+window.onscroll = () => window.scrollY > 200 ? navigation.classList.add('solid') : navigation.classList.remove('solid')
+
+
+
+new Vivus('svg1', { duration: 200 });
+new Vivus('svg2', { duration: 200 });
+new Vivus('svg3', { duration: 200 });
+new Vivus('svg4', { duration: 200 });
